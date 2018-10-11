ef45d8f84470f3b4ce9b38084f0d4ddd78546978
antojrd@gmail.com <antojrd@gmail.com>
2018-10-11 13:49:48 -0500

    Ejemplo template pago booking

---
+++ paybooking.php
@@ -0,0 +1,26 @@
+<?php
+
+ini_set('display_errors', 1);
+include_once 'conf/constant.php';
+include Booking;
+
+$show_list = $_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['booking']);
+
+
+// Show list
+if ($show_list){
+
+    $is_authenticated = Booking::verify();
+    if ($is_authenticated){
+
+        $booking = Booking::get_by_code($_GET['booking']);
+
+        $smarty->display(Template_Dir.'/admin/login.tpl');
+        
+    }
+    else{
+        Login::navigate('index');
+    }
+}
+
+
